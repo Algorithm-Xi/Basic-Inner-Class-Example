99c99785d66a1ddd9ad4e1d477c9988ebafa1756
Dante <2donta.dw@gmail.com>
2016-08-06 06:12:33 -0400

    initial commit

---
+++ src/DrEvil.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Donta on 8/6/16.
+ */
+public class DrEvil {
+
+    public static void main(String[] args){
+
+
+    }
+
+
+}
---
+++ src/Spider.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Donta on 8/6/16.
+ */
+public class Spider {
+
+    public static void main(String[] args) {
+
+        Web a = new Web();
+        Web.Venom obj = a.new Venom();
+        obj.show();
+
+    }
+
+
+
+}
+
+    class Web {
+        int t;
+        String red;
+
+
+
+         class Venom {
+
+            int k;
+
+            String green;
+
+
+            public void show() {
+                System.out.println("yikes");
+
+            }
+
+
+
+        }
+
+    }
+
+
